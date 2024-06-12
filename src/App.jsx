0942383d57e8aa813311f9241dcdef9589567ff5
selva <selvanithish2014@gmail.com>
2024-06-12 11:26:08 +0530

    Simpole

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>Counter</h1>
+    <div className="">
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+    <button onClick={() => setCount(0)}>Reset</button>
+    </>
+  )
+}
+
+export default App
